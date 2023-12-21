6634d0bd26f9ac36aba797bfeb1de70b72121350
Artur Michalak <arturmichalak96@gmail.com>
2023-12-21 22:08:23 +0100

    vercel fix

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -11,8 +11,6 @@ export default async function Home({ params }: { params: { lang: string } }) {
   return (
     <Hydrate state={dehydratedState}>
       <main className="flex-grow">
-        <div>API {process.env.API_URL}</div>
-        <div>VERCEL {process.env.VERCEL_URL}</div>
         <List lang={params.lang} />
       </main>
     </Hydrate>
